66f1ef07565666fb1443f8b3073a446aa38d96a4
ShlokD <shlokpdesai@gmail.com>
2017-10-02 22:45:30 -0700

    'Variables'

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -4,10 +4,10 @@ def sieve(limit):
     array_of_primes = []
     if limit >= 2:
         array_of_primes.append(2)
-        multiples = set()
+        non_primes = set()
         for i in range(3, limit + 1, 2):
-            if i not in multiples:
+            if i not in non_primes:
                 array_of_primes.append(i)
-                multiples.update(range(int(math.pow(i, 2)), limit, i))
+                non_primes.update(range(int(math.pow(i, 2)), limit, i))
 
     return array_of_primes

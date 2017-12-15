640a55ca38e167e3c5eb98b61e1ac730f5d68534
ShlokD <shlokpdesai@gmail.com>
2017-12-14 18:45:46 -0800

    Roman numerals

---
+++ roman-numerals/roman_numerals.py
@@ -0,0 +1,23 @@
+numeral_map = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+  }
+
+def numeral(number):
+    romanNumeral = ''
+    for numeral in numeral_map:
+        while(number >= numeral_map[numeral]):
+            romanNumeral += numeral
+            number -= numeral_map[numeral]
+    return romanNumeral
